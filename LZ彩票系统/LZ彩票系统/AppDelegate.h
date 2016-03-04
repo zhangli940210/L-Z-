2ae02dea084018e074eadbfe6025219763e7188e
gj <gj@itcast.cn>
2016-03-04 18:04:18 +0800

    初始化项目

---
+++ LZ彩票系统/LZ彩票系统/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LZ彩票系统
+//
+//  Created by apple on 16/3/4.
+//  Copyright © 2016年 m14a.cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LZ彩票系统/LZ彩票系统/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LZ彩票系统
+//
+//  Created by apple on 16/3/4.
+//  Copyright © 2016年 m14a.cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
